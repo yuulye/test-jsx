90acfbfecd50a1b0a720793205c460e16e173d29
yuulye <yuulye@gmail.com>
2019-07-31 15:38:51 +0000

    Updates

---
+++ precache-manifest.45080b44581cd77522fa40ac93c4873f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7be9682364f673ebf255536a4cc287fc",
+    "url": "/test-jsx/index.html"
+  },
+  {
+    "revision": "4d64e63e970205675281",
+    "url": "/test-jsx/static/js/2.d93c53f0.chunk.js"
+  },
+  {
+    "revision": "667498e1012ab980340e",
+    "url": "/test-jsx/static/js/main.784c86d7.chunk.js"
+  },
+  {
+    "revision": "dc4c67d9c14b5b286432",
+    "url": "/test-jsx/static/js/runtime~main.d1a64204.js"
+  }
+]);
